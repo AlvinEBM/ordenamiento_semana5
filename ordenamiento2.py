c6956a617949f763ad5857a1269a69262d4d909d
AlvinEBM <alvinelvis17@gmail.com>
2025-04-25 21:50:58 -0500

    modificaciones

---
+++ ordenamiento2.py
@@ -0,0 +1,11 @@
+def ordenamiento(arr):
+    #inicio=1
+    for inicio in range(1,len(arr)):
+     final=inicio
+     while final >0 and arr[final-1]>arr[final]:
+        arr[final],arr[final-1]=arr[final-1],arr[final]
+        final=final-1
+
+a=[9,8,7,6,5,4,3,2,1]
+ordenamiento(a)
+print(a)     
