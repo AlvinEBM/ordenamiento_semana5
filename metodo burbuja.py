c46827f58056050c1fddbc6d23085f798fc79f59
AlvinEBM <alvinelvis17@gmail.com>
2025-04-25 22:32:41 -0500

    222

--- metodo burbuja.py
+++ metodo burbuja.py
@@ -1,1 +1,10 @@
+def ordena_bur(lista):
+    for inicio in range(1,len(lista)):
+        for final in range(0,len(lista)-1):
+            if lista[final]>lista[final+1]:
+                #inicio=final
+                lista[final],lista[final+1]=lista[final+1],lista[final]
 
+a=[9,8,7,6,5,4,3,2,1]
+ordena_bur(a)
+print(a)     
